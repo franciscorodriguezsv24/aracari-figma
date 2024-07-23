67ca948b2e2644829a7e9e2f11275f00c820514e
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2024-07-22 22:02:13 -0600

    created routes and installed libraries

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, useRoutes } from 'react-router-dom'
+import About from '../About'
+import Home from '../Home'
+import NotFound from '../NotFound'
+import './App.css'
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {path:'/', element: <Home/>},
+    {path:'/about', element: <About/>},
+    {path:'/*', element: <NotFound/>}
+  ])
+
+  return routes
+}
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
