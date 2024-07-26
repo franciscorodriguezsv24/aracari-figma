a5b6dc486875b69d9cead03b9bd0483619a3af17
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2024-07-25 18:53:17 -0600

    created home responsive

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,42 +6,43 @@ import Instagram from '../Icons/Instagram';
 import Xtwitter from '../Icons/Xtwitter';
 
 const Footer = () => {
-  return (
-    <footer className={StyleCss.footerContainer}>
-        <div className={StyleCss.footerElementContainer}>
-            <a className={StyleCss.footerSectionText}>
-            Más sobre nosotros
-            </a>
-            <ul className={StyleCss.footerItemsText}>
-                <li>¿Quiénes somos?</li>
-                <li>Roadmap del proyecto</li>
-                <li>Relaciones con inversionistas</li>
-                <li>Conoce a nuestro equipo </li>
-            </ul>
+    return (
+        <div className={StyleCss.container}>
+            <footer className={StyleCss.footerContainer}>
+                <div className={StyleCss.footerElementContainer}>
+                    <p className={StyleCss.footerSectionText}>
+                    Más sobre nosotros
+                    </p>
+                    <ul className={StyleCss.footerItemsText}>
+                        <a href='/'>¿Quiénes somos?</a>
+                        <a href='/'>Roadmap del proyecto</a>
+                        <a href='/'>Relaciones con inversionistas</a>
+                        <a href='/'>Conoce a nuestro equipo </a>
+                    </ul>
+                </div>
+                <div className={StyleCss.footerElementContainer}>
+                    <p className={StyleCss.footerSectionText}>
+                    Legal
+                    </p>
+                    <ul className={StyleCss.footerItemsText}>
+                        <a href='/'>Términos y condiciones de uso</a>
+                        <a href='/'>Uso ético de datos</a>
+                        <a href='/'>Política de uso aceptable</a>
+                        <a href='/'>Configuración de cookies</a>
+                    </ul>
+                </div>
+                <div className={StyleCss.footerElementContainerIcom}>
+                    <p className={StyleCss.footerIconSectionText}>Siguenos en:</p>
+                    <div className={StyleCss.footerIconContainer}>
+                        <a href='/'><Facebook/></a>
+                        <a href='https://www.instagram.com/aracaristudios/' target="blank"><Instagram/></a>
+                        <a href='/'><Xtwitter/></a>
+                    </div>
+                </div>
+            </footer>
+            <hr className={StyleCss.linearGradient}/>
         </div>
-        <div className={StyleCss.footerElementContainer}>
-            <a className={StyleCss.footerSectionText}>
-            Legal
-            </a>
-            <ul className={StyleCss.footerItemsText}>
-                <li>Términos y condiciones de uso</li>
-                <li>Uso ético de datos</li>
-                <li>Política de uso aceptable</li>
-                <li>Configuración de cookies</li>
-            </ul>
-        </div>
-        <div className={StyleCss.footerElementContainer}>
-            <a className={StyleCss.footerSectionText}>Siguenos en:</a>
-            <div>
-                <a>
-                <Facebook/>
-                </a>
-                <a><Instagram/></a>
-                <a><Xtwitter/></a>
-            </div>
-        </div>
-    </footer>
-  )
+    )
 }
 
 export default Footer
